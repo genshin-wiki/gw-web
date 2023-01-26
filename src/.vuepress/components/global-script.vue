157d0d712667a246c8fbce8f33b9461bdce2881e
真心 <ceo@iqianye.cn>
2023-01-26 23:02:13 +0800

    add: myhk music player

--- src/.vuepress/components/global-script.vue
+++ src/.vuepress/components/global-script.vue
@@ -4,7 +4,13 @@ import { onMounted } from 'vue';
 onMounted(() => {
   const autoGray = document.createElement("script");
   autoGray.setAttribute("src", "https://cdn.armoe.cn/static/js/autoGray.js")
+  const player = document.createElement("script");
+  player.setAttribute("id", "myhk");
+  player.setAttribute("src", "https://myhkw.cn/api/player/167470446269");
+  player.setAttribute("key", "167470446269");
+  player.setAttribute("m", "1");
   document.body.appendChild(autoGray);
+  document.body.appendChild(player);
 })
 </script>
 
